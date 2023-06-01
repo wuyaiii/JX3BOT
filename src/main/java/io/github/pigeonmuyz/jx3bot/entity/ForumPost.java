361a14b08d00e15d875db8484701cecc4eadb168
清晨 <muyz0319@icloud.com>
2023-06-01 13:37:00 +0800

    八卦实体类

---
+++ src/main/java/io/github/pigeonmuyz/jx3bot/entity/ForumPost.java
@@ -0,0 +1,54 @@
+package io.github.pigeonmuyz.jx3bot.entity;
+
+/**
+ * 八卦推送用的实体类！
+ */
+public class ForumPost {
+    String server;
+    String name;
+    String title;
+    String url;
+    String date;
+
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUri() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
+
